17036e3e27ae27bd9da7a7322615ed2af8ec0ae6
Владимир Чеховский <126843806+bakafox@users.noreply.github.com>
2024-03-06 03:58:47 +0000

    Переписано на C++11 (эксперимент)

--- main.cpp
+++ main.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
-#define MAX 1000000
+#include <vector>
+#define MAX 10000000
+
+using namespace std;
 
 int main() {
     // передаётся как аргумент при сборке
 #ifdef USE_FLOAT
-    float numbers[MAX];
+    vector<float> numbers(MAX);
     float sum = 0;
 #else
-    double numbers[MAX];
+    vector<double> numbers(MAX);
     double sum = 0;
 #endif
 
     // СНАЧАЛА заполняем массив, ЗАТЕМ суммируем (по условию)
     for (int i = 0; i < MAX; i++) {
-        numbers[i] = sin(2*3.1415926 * i/MAX);
+        numbers[i] = sin(2 * M_PI * i/MAX);
     }
     for (int j = 0; j < MAX; j++) {
         sum += numbers[j];
     }
 
 #ifdef USE_FLOAT
-    printf("\nFloat: ");
+    cout << "Float: " << fixed << setprecision(20) << sum << endl;
 #else
-    printf("\nDouble: ");
+    cout << "Double: " << fixed << setprecision(20) << sum << endl;
 #endif
-    printf("%.20f\n", sum);
 }
